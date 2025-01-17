d196e3ad1447edaaf260f656a495589f105834da
Emily <emily200913@hotmail.com>
2025-01-16 21:06:48 -0300

    Atualizaçao do exercicio

---
+++ Exercicio 6/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Exercicio_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota_final = 70;
+            string resultado;
+
+            if (nota_final >= 60)
+            {
+                resultado = "Aprovado";
+            }
+            else
+            {
+                resultado = "Reprovado";
+            }
+            
+            
+            Console.WriteLine("O aluno foi {0}.",resultado);
+
+
+
+
+            
+        }
+    }
+}
