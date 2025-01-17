09307f04aaec356087421bb0087cea1345f17345
Emily <emily200913@hotmail.com>
2025-01-16 21:23:20 -0300

    adicionado recuperação

--- Exercicio 6/Program.cs
+++ Exercicio 6/Program.cs
@@ -4,12 +4,16 @@
     {
         static void Main(string[] args)
         {
-            double nota_final = 70;
+            double nota_final = 30;
             string resultado;
 
             if (nota_final >= 60)
             {
                 resultado = "Aprovado";
+            }
+            else if (nota_final >= 40)
+            {
+                resultado = "Recuperação";
             }
             else
             {
