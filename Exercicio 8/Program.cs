5a73a34709e0dc93183ea15bd664acb6d7ae715d
Emily <emily200913@hotmail.com>
2025-01-21 19:32:37 -0300

    Exercicio 8, atribuindo && e || teste do triangulo

---
+++ Exercicio 8/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.Design;
+
+namespace Exercicio_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ladoA, ladoB, ladoC;
+
+            Console.WriteLine("Programa para testar triangulos");   
+
+            Console.WriteLine("Digite o valor do Lado A: ");
+            ladoA=Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Digite o valor do Lado B: ");
+            ladoB =Convert.ToInt32(Console.ReadLine());
+           
+            Console.WriteLine("Digite o valor do Lado C: ");
+            ladoC =Convert.ToInt32(Console.ReadLine());
+
+            if ((ladoA < ladoB + ladoC) && (ladoB < ladoA + ladoC) && (ladoC < ladoA + ladoC))
+            {
+                Console.WriteLine("os valores informados formam um triangulo");
+            }
+
+            else
+            {
+                Console.WriteLine("Os valores não formam um triangulo.");
+            }
+
+             
+
+
+        }
+    }
+}
---
+++ exercicio 7/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace exercicio_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            string resultado;
+
+            if(numero % 2);
+
+
+
+            
+
+        }
+    }
+}
