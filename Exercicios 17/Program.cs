b29c71aaeda1d32d953ec00b77bba71153197c36
Emily <emily200913@hotmail.com>
2025-01-28 20:03:12 -0300

    exercicio senha

---
+++ Exercicios 17/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Exercicios_17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 5;
+
+            while (num < 5)
+            {
+                num++;
+                Console.WriteLine("curso C#");
+
+            }
+
+            do
+            {
+                num++;
+                Console.WriteLine("curso C#");
+                while (num < 5) ;
+            }
+    }   }
+}  
---
+++ exercicio senha/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace exercicio_senha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string senha = "123"; //senha correta
+            string senhaDigitada; // senha digitada ´pelo usuario
+            int tentativas = 0;  //contador de tentativas
+           //loop para solicitar a senha ao usuario
+            do
+            {
+                Console.Clear(); 
+                Console.Write("Digite a senha: ");
+                senhaDigitada = Console.ReadLine();
+                tentativas++; // incrementa o contador de tentativas
+
+                //verifica se a senha digitada é diferente da senha correta
+                if (senhaDigitada != senha && tentativas < 3)
+                {
+                    Console.WriteLine("Senha incorreta!");
+                    Console.WriteLine("você tem ainda" + (3 - tentativas) + "tentativa(s)");
+                    Console.ReadKey();
+                }
+                else  if (senhaDigitada != senha && tentativas == 3)    
+                {
+                    Console.WriteLine("Senha incorreta! Seu acesso está bloqueado!");
+                    return;
+                }
+            } while (senhaDigitada != senha);
+
+            Console.Clear() ;
+            Console.WriteLine("Senha correta! - Tentativas:" + tentativas);
+
+           
+
+            
+             
+        }
+    }
+}
