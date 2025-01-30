bd64856ce472594c57a624116c790549ec4c3f45
Emily <emily200913@hotmail.com>
2025-01-30 20:55:25 -0300

    metodos

---
+++ Metodos/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+
+namespace Metodos
+{
+    internal class Program
+    {
+        static void Main(string[] args) // metodo Main principal, faz rodar o codigo.
+        {
+            Saudaçao("Emily");
+            Soma(10, 20);
+        }
+        static void Saudaçao(string nome)// COLOCAR SEMPRE DENTRO DA CHAVE INTERNAL CLASS PROGRAM. (metodo saudação)
+        {
+            Console.WriteLine($"ola {nome} seja bem vindo"); // Exibe uma mensagem de saudação 
+        }  
+
+        static void Soma(int a, int b) // metodo soma 
+        {
+            int resultado = a + b;
+            Console.WriteLine($"A soma de {a} + {b}");  
+           
+        }
+    }
+}
