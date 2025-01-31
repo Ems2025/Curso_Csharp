f2d3f297abd334941227835de3abc1b8b4dfcab7
Emily <emily200913@hotmail.com>
2025-01-30 21:31:43 -0300

    area triangulo

---
+++ area triangulo/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace area_triangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("=====================");
+            Console.WriteLine("Area do triangulo");     
+            Console.WriteLine("=====================");
+            Console.WriteLine();
+
+            Console.Write("Digite a base do triangulo (m): ");
+            double Base1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.Write("Digite a altura do triangulo (m): ");
+            double Altura = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+
+            Areatriangulo(Base1, Altura);
+
+
+
+        }
+
+        static void Areatriangulo(double b, double A)
+        {
+            double area = (b * A) / 2;
+            Console.WriteLine($"A area do triangulo de {b} x a base de {A} resulta em {area}");
+        }
+    }
+}
