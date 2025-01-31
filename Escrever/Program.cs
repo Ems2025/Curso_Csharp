b027396042fefdffd4fb2f30088d292fd17a886f
Emily <emily200913@hotmail.com>
2025-01-30 21:49:31 -0300

    texto

---
+++ Escrever/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Escrever
+{
+    internal class Program
+    {
+        static void Escrever(string texto)
+        {
+            int tam = texto.Length + 4;
+            string Linha = new('*', tam);
+            string textocentralizado = texto.PadLeft(tam - 2). PadRight(tam - 2);
+            Console.WriteLine(Linha);
+            Console.WriteLine(textocentralizado);
+            Console.WriteLine(Linha);
+        }
+       
+        static void Main(string[] args)
+        {
+            Escrever("Emily Michelle de Sousa");
+            Console.WriteLine();
+            Escrever("Curso C#");
+            Console.WriteLine();
+            Escrever("Eu amo bike s2");
+            Console.WriteLine();
+
+
+
+        }
+    }
+}
